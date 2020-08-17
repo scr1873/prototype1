12fe87d4ee7f59ca9866d3056bf0c5707de4086a
scr1873 <scr1873@stacmail.net>
2020-08-17 18:08:01 +1200

    made it actually work (skill event handler added)

--- Prototype1.py
+++ Prototype1.py
@@ -1,220 +1,221 @@
-import boto3
-ddb = boto3.client("dynamodb")
-from ask_sdk_core.skill_builder import CustomSkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
-from ask_sdk_core.handler_input import HandlerInput
+def handler(event, context):
+    import boto3
+    ddb = boto3.client("dynamodb")
+    from ask_sdk_core.skill_builder import CustomSkillBuilder
+    from ask_sdk_core.dispatch_components import AbstractRequestHandler
+    from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+    from ask_sdk_core.handler_input import HandlerInput
 
-from ask_sdk_model import Response
+    from ask_sdk_model import Response
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
-class LaunchRequestHandler(AbstractRequestHandler):
-    """Handler for Skill Launch."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-
-        return ask_utils.is_request_type("LaunchRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = ""
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
 
-class ReportDataIntentHandler(AbstractRequestHandler):
-    """Handler for Report Data Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("ReportDataIntent")(handler_input)
+    class LaunchRequestHandler(AbstractRequestHandler):
+        """Handler for Skill Launch."""
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        
-        slots = handler_input.request_envelope.request.intent.slots
-        datatype = slots["datatype"].value
-        name = slots["name"].value
-        value = slots["value"].value
-        unit = slots["unit"].value
-        try:
-            data = ddb.put_item(
-                TableName="Health",
-                Item={
-                    'Datatype': {
-                        'S': datatype
-                    },
-                    'Name': {
-                        'S': name
-                    },
-                    'Value': {
-                        'S': value
-                    },
-                    'Unit': {
-                        'S': unit
-                    },
-                    'userID': {
-                        'S': 23498
-                    }
-                }
+            return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
+            speak_output = ""
+
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    .ask(speak_output)
+                    .response
             )
-        except BaseException as e:
-            print(e)
-            raise(e)
-        
-        
-        # get device id
-        sys_object = handler_input.request_envelope.context.system
-        device_id = sys_object.device.device_id
 
-        # get Alexa Settings API information
-        api_endpoint = sys_object.api_endpoint
-        api_access_token = sys_object.api_access_token
-        
-         # construct systems api timezone url
-        url = '{api_endpoint}/v2/devices/{device_id}/settings/System.timeZone'.format(api_endpoint=api_endpoint, device_id=device_id)
-        headers = {'Authorization': 'Bearer ' + api_access_token}
 
-        userTimeZone = ""
-        try:
-            r = requests.get(url, headers=headers)
-            res = r.json()
-            logger.info("Device API result: {}".format(str(res)))
-            userTimeZone = res
-        except Exception:
-            handler_input.response_builder.speak("There was a problem connecting to the service")
-            return handler_input.response_builder.response
+    class ReportDataIntentHandler(AbstractRequestHandler):
+        """Handler for Report Data Intent."""
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
+            return ask_utils.is_intent_name("ReportDataIntent")(handler_input)
+
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
             
-        
-        # getting the current date with the time
-        now_time = datetime.now(timezone(userTimeZone))
-        
-        speak_output = "added {} at {} for {} {} {}".format(datatype, value, name, unit, nowtime)
+            slots = handler_input.request_envelope.request.intent.slots
+            datatype = slots["datatype"].value
+            name = slots["name"].value
+            value = slots["value"].value
+            unit = slots["unit"].value
+            try:
+                data = ddb.put_item(
+                    TableName="Health",
+                    Item={
+                        'Datatype': {
+                            'S': datatype
+                        },
+                        'Name': {
+                            'S': name
+                        },
+                        'Value': {
+                            'S': value
+                        },
+                        'Unit': {
+                            'S': unit
+                        },
+                        'userID': {
+                            'S': 23498
+                        }
+                    }
+                )
+            except BaseException as e:
+                print(e)
+                raise(e)
+            
+            
+            # get device id
+            sys_object = handler_input.request_envelope.context.system
+            device_id = sys_object.device.device_id
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
+            # get Alexa Settings API information
+            api_endpoint = sys_object.api_endpoint
+            api_access_token = sys_object.api_access_token
+            
+            # construct systems api timezone url
+            url = '{api_endpoint}/v2/devices/{device_id}/settings/System.timeZone'.format(api_endpoint=api_endpoint, device_id=device_id)
+            headers = {'Authorization': 'Bearer ' + api_access_token}
 
+            userTimeZone = ""
+            try:
+                r = requests.get(url, headers=headers)
+                res = r.json()
+                logger.info("Device API result: {}".format(str(res)))
+                userTimeZone = res
+            except Exception:
+                handler_input.response_builder.speak("There was a problem connecting to the service")
+                return handler_input.response_builder.response
+                
+            
+            # getting the current date with the time
+            now_time = datetime.now(timezone(userTimeZone))
+            
+            speak_output = "added {} at {} for {} {} {}".format(datatype, value, name, unit, nowtime)
 
-class HelpIntentHandler(AbstractRequestHandler):
-    """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "You can say hello to me! How can I help?"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
-
-class CancelOrStopIntentHandler(AbstractRequestHandler):
-    """Single handler for Cancel and Stop Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "Goodbye!"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
-
-
-class SessionEndedRequestHandler(AbstractRequestHandler):
-    """Handler for Session End."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
-        # Any cleanup logic goes here.
-
-        return handler_input.response_builder.response
-
-
-class IntentReflectorHandler(AbstractRequestHandler):
-    """The intent reflector is used for interaction model testing and debugging.
-    It will simply repeat the intent the user said. You can create custom handlers
-    for your intents by defining them above, then also adding them to the request
-    handler chain below.
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("IntentRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        intent_name = ask_utils.get_intent_name(handler_input)
-        speak_output = "You just triggered " + intent_name + "."
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                    .response
+            )
 
 
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Generic error handling to capture any syntax or routing errors. If you receive an error
-    stating the request handler chain is not found, you have not implemented a handler for
-    the intent being invoked or included it in the skill builder below.
-    """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
+    class HelpIntentHandler(AbstractRequestHandler):
+        """Handler for Help Intent."""
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
+            return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
 
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
+            speak_output = "You can say hello to me! How can I help?"
 
-        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
-# The SkillBuilder object acts as the entry point for your skill, routing all request and response
-# payloads to the handlers above. Make sure any new handlers or interceptors you've
-# defined are included below. The order matters - they're processed top to bottom.
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    .ask(speak_output)
+                    .response
+            )
 
 
-sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
+    class CancelOrStopIntentHandler(AbstractRequestHandler):
+        """Single handler for Cancel and Stop Intent."""
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
+            return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                    ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
 
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(ReportDataIntentHandler())
-sb.add_request_handler(HelpIntentHandler())
-sb.add_request_handler(CancelOrStopIntentHandler())
-sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
+            speak_output = "Goodbye!"
 
-sb.add_exception_handler(CatchAllExceptionHandler())
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    .response
+            )
 
-lambda_handler = sb.lambda_handler()
+
+    class SessionEndedRequestHandler(AbstractRequestHandler):
+        """Handler for Session End."""
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
+            return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
+
+            # Any cleanup logic goes here.
+
+            return handler_input.response_builder.response
+
+
+    class IntentReflectorHandler(AbstractRequestHandler):
+        """The intent reflector is used for interaction model testing and debugging.
+        It will simply repeat the intent the user said. You can create custom handlers
+        for your intents by defining them above, then also adding them to the request
+        handler chain below.
+        """
+        def can_handle(self, handler_input):
+            # type: (HandlerInput) -> bool
+            return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+        def handle(self, handler_input):
+            # type: (HandlerInput) -> Response
+            intent_name = ask_utils.get_intent_name(handler_input)
+            speak_output = "You just triggered " + intent_name + "."
+
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                    .response
+            )
+
+
+    class CatchAllExceptionHandler(AbstractExceptionHandler):
+        """Generic error handling to capture any syntax or routing errors. If you receive an error
+        stating the request handler chain is not found, you have not implemented a handler for
+        the intent being invoked or included it in the skill builder below.
+        """
+        def can_handle(self, handler_input, exception):
+            # type: (HandlerInput, Exception) -> bool
+            return True
+
+        def handle(self, handler_input, exception):
+            # type: (HandlerInput, Exception) -> Response
+            logger.error(exception, exc_info=True)
+
+            speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    .ask(speak_output)
+                    .response
+            )
+
+    # The SkillBuilder object acts as the entry point for your skill, routing all request and response
+    # payloads to the handlers above. Make sure any new handlers or interceptors you've
+    # defined are included below. The order matters - they're processed top to bottom.
+
+
+    sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
+
+    sb.add_request_handler(LaunchRequestHandler())
+    sb.add_request_handler(ReportDataIntentHandler())
+    sb.add_request_handler(HelpIntentHandler())
+    sb.add_request_handler(CancelOrStopIntentHandler())
+    sb.add_request_handler(SessionEndedRequestHandler())
+    sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
+    sb.add_exception_handler(CatchAllExceptionHandler())
+
+    lambda_handler = sb.lambda_handler()
